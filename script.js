f7afc7c36a5cf2ad7a58da5e1f3b17700b73972e
fulmack <sylvainvantieghem@gmail.com>
2019-04-10 12:07:45 +0200

    add in js function buttons/actifId/display and debug view image

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 'use strict';
   //let elem = document.getElementsByTagName('img')[0];
   //elem.classList.toggle("slide");
-let i = 0;
-function nextSlide() {
-  let elem = document.getElementsByTagName('img')[0];
-if(i > elem.lenght){
-    console.log("loop for");
-    //elem.classList.toggle("slide");
-     elem[i].style.display = "none";
-i++;
-    //elem.classList.toggle("img");
+let slideShow = 1;
+display(slideShow);
+
+function buttons(n) {
+  display(slideShow += n);
+}
+
+function actifId(n) {
+  display(slideShow = n);
+}
+
+function display(n) {
+
+  let imgSlide = document.getElementsByClassName("slide");
+  let id = document.getElementsByClassName("demo");
+  /*console.log(n);*/
+    (n > imgSlide.length) ? slideShow = 1 : null;
+    n < 1 ? slideShow = imgSlide.length : null;
+    /*console.log(slideShow.length);*/
+    for (let i = 0; i < imgSlide.length; i++) {
+      // To replace visible hidden
+      imgSlide[i].style.opacity = "0";
+      /*console.log(imgSlide[i]);*/
+    }
+
+    for (let i = 0; i < id.length; i++) {
+
+      id[i].className = id[i].className.replace(" numbers", "");
+    }
+  //  To replace visible true
+  imgSlide[slideShow-1].style.opacity = "1";
+
+  id[slideShow-1].className += " numbers";
 }
 
 
-}
 
