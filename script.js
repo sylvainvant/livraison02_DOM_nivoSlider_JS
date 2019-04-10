8b8a46defabb970a436673acfe2557a5b56691b4
fulmack <sylvainvantieghem@gmail.com>
2019-04-10 09:37:43 +0200

    change file in img + add tag html

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 'use strict';
-
+  //let elem = document.getElementsByTagName('img')[0];
+  //elem.classList.toggle("slide");
+let i = 0;
 function nextSlide() {
   let elem = document.getElementsByTagName('img')[0];
-  //elem.classList.toggle("slide");
-
-    elem.classList.toggle("slide");
+if(i > elem.lenght){
+    console.log("loop for");
+    //elem.classList.toggle("slide");
+     elem[i].style.display = "none";
+i++;
+    //elem.classList.toggle("img");
+}
 
 
 }
